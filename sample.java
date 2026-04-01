7942295ecd44deb997aa0ea756d6690f93be9248
manasvig05 <manasvigawde2005@gmail.com>
2026-04-01 12:34:18 +0530

    Create sample.java

--- sample.java
+++ sample.java
@@ -2,6 +2,6 @@ class sample
   {
     public static void main(String args[])
     {
-      System.out.println("This is a sample file.")
+      System.out.println("This is a sample file.");
     }
   }
